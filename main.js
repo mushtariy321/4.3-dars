fef23555fd1c1a58aff56a91c7c4166ca1ffe4ea
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-04-17 16:35:10 +0300

    done

---
+++ main.js
@@ -0,0 +1,20 @@
+// let num = 5.7
+// console.log (Math.floor(Math.random()*100));
+
+
+// console.log(true !== false);
+
+
+// let num1 = +prompt('son kiriting')
+// let num = +prompt('son kiriting!')
+// console.log (Math.round(num + num1));
+
+
+let number = (Math.floor(Math.random() * 100))
+alert(`Random son: ${number} uni 2ga ko'paytiramiz`);
+alert(`javob:${number * 2}`);
+
+alert(`Random son: ${number} uni 2ga bo'lamiz`);
+alert(`javob: ${number / 2}`);
+
+alert(`Random son: ${number} uni 2ga ko'paytiramiz \njavob:${number * 2} \nRandom son: ${number} uni 2ga bo'lamiz \njavob: ${number / 2}`)
